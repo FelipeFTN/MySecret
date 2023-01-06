e5f646df9e11403d497f15cbbb6de86f1b97cebc
FelipeFTN <felipe.tenorio123456@gmail.com>
2023-01-06 16:29:59 -0300

    ⭐ design pattern

--- main.rb
+++ main.rb
@@ -1,8 +1,5 @@
-require_relative "handler/cryptography"
 require_relative "handler/constants"
-require_relative "handler/system"
-require_relative "handler/file"
-require_relative "handler/note"
+require_relative "handler/main"
 
 # Create MySecret folder
 Dir.mkdir(MYSECRET_DIRECTORY) unless File.exists?(MYSECRET_DIRECTORY)
@@ -41,49 +38,7 @@ while true
   end
 
   # Handle options
-  if option == 1
-    # Get file name & secret key
-    file_path = get_file()
-    secret_key = get_secret_key()
+  handler(option)
 
-    write_note(file_path, secret_key)
-  elsif option == 2
-    # Get file name & secret key
-    file_path = get_file()
-    secret_key = get_secret_key()
-
-    read_note(file_path, secret_key)
-  elsif option == 3
-    # Get file name & secret key
-    file_path = get_file()
-    secret_key = get_secret_key()
-
-    edit_note(file_path, secret_key)
-  elsif option == 4
-    # Get file name
-    file_path = get_file()
-    secret_key = get_secret_key("deleting confirmation secret_key: ")
-
-    delete_note(file_path, secret_key)
-  elsif option == 5
-    # Get file name & secret key
-    file_path = get_file()
-    secret_key = get_secret_key()
-
-    encrypt_note(file_path, secret_key)
-  elsif option == 6
-    # Get file name & secret key
-    file_path = get_file()
-    secret_key = get_secret_key()
-
-    decrypt_note(file_path, secret_key)
-  elsif option == 7
-    # Get file name & secret key
-    file_path = get_file()
-    secret_key = get_secret_key()
-    new_secret_key = get_secret_key("new secret key: ")
-
-    change_secret(file_path, secret_key, new_secret_key)
-  end
   puts `clear`
 end
