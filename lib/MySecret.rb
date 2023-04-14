dfecc23200e17ea76944b99538b8dacffaa9b4da
FelipeFTN <felipe.tenorio123456@gmail.com>
2023-04-14 15:34:30 -0300

    🪐 MySecret v2.0.1

--- lib/MySecret.rb
+++ lib/MySecret.rb
@@ -9,25 +9,27 @@ require_relative "MySecret/handler/main"
 module MySecret
   class Error < StandardError; end
 
-  # Create MySecret folder
-  Dir.mkdir(CONFIG_DIRECTORY) unless File.exist?(CONFIG_DIRECTORY)
-  Dir.mkdir(MYSECRET_DIRECTORY) unless File.exist?(MYSECRET_DIRECTORY)
-  Dir.mkdir(NOTES_DIRECTORY) unless File.exist?(NOTES_DIRECTORY)
+  def self.run()
+    # Create MySecret folder
+    Dir.mkdir(CONFIG_DIRECTORY) unless File.exist?(CONFIG_DIRECTORY)
+    Dir.mkdir(MYSECRET_DIRECTORY) unless File.exist?(MYSECRET_DIRECTORY)
+    Dir.mkdir(NOTES_DIRECTORY) unless File.exist?(NOTES_DIRECTORY)
 
-  while true
-    # List notes
-    display_notes()
+    while true
+      # List notes
+      display_notes()
 
-    # Display menu options
-    option = options_menu()
+      # Display menu options
+      option = options_menu()
 
-    # Input Validation
-    validate_options(option) ? nil : return
+      # Input Validation
+      validate_options(option) ? nil : return
 
-    # Handle options
-    handler(option) 
+      # Handle options
+      handler(option)
 
-    # Clear terminal
-    puts `clear`
+      # Clear terminal
+      puts `clear`
+    end
   end
 end
